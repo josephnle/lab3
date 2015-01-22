ac2c88d4bd0d7fe23d133bd2a2ec538b0cda291b
Joseph Le <josephle@me.com>
2015-01-22 13:09:04 -0800

    JavaScript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,17 +2,49 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
-})
+  initializePage();
+});
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+  $("#testjs").click(function(e) {
+    $('.jumbotron h1').text("Javascript is connected");
+    $(this).text("Clicked");
+    $(".jumbotron p").addClass('active');
+  });
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+  // Add any additional listeners here
+  // example: $("#div-id").click(functionToCall);
+  $("a.thumbnail").click(projectClick);
+
+  $("#submitBtn").click(function(e) {
+    var project = $('#project').val();
+    var width = $('#width').val();
+
+    var newText = $('#description').val();
+    $(project + " .project-description").text(newText);
+
+    $(project).width(width);
+  });
+}
+
+function projectClick(e) {
+  // Cancel the default action, which prevents the page from reloading
+  e.preventDefault();
+
+  // In an event listener, $(this) is the element that fired the event
+  var projectTitle = $(this).find("p").text();
+  var jumbotronHeader = $(".jumbotron h1");
+  jumbotronHeader.text(projectTitle);
+
+  var containingProject = $(this).closest(".project"); 
+  var description = $(containingProject).find(".project-description");
+  if (description.length == 0) { 
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+  }
+  else { 
+		description.fadeOut();
+  }
 }
